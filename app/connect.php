6e47630aa69708bef5c3aed1b60e0671c4dda959
ecobricksinfra-lgtm <ecobricksinfra@gmail.com>
2026-01-29 09:52:40 +0530

    delete

--- app/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost","root","","spacezoneindia");
-//$connect = mysqli_connect("localhost","spacezon_india_Dev","SpaceZoneDev@2025$$","spacezon_india");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
